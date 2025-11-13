9b449aab662afde3e447bddfecc4790070c5d286
dakjebbin <jebbinp@gmail.com>
2025-11-13 05:54:54 +0100

    con

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 <<<<<<< HEAD
+<<<<<<< HEAD
 import {Login} from "./Login"
 =======
 import {Login} from "./Login.tsx"
 >>>>>>> 6c4e3d4 (login)
+=======
+import {Login} from "./Login"
+>>>>>>> 8c6ebdf (con)
 
 const page = () => {
   return (
