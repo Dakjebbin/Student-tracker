9a533250020e8803c80b6e787b7505f0cbd8d979
dakjebbin <jebbinp@gmail.com>
2025-11-17 15:48:12 +0100

    done

--- lib/cors.ts
+++ lib/cors.ts
@@ -3,7 +3,7 @@ export function corsResponse(data: any, status = 200) {
       status,
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "https://student-tracker-orcin.vercel.app",
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
       },
